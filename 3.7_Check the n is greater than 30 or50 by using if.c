f4467d4150c6db7769c75237c1949cc8d5f88ffb
Akila-Rahman <73312109+Akila-Rahman@users.noreply.github.com>
2024-12-10 03:36:27 +0600

    Add files via upload

---
+++ 3.7_Check the n is greater than 30 or50 by using if.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    n= 10;
+
+    if(n < 30) {
+         printf("n is greater than 30.\n");
+    }
+    if(n < 50) {
+         printf("n is greater than 50.\n");
+    }
+
+    return 0;
+
+}
+
